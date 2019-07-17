9f10ac2048000fdc22c3741df1e306118e50914f
Le Trong <le.trong@bxmon.com>
2019-07-17 18:17:20 -0400

    Export constants

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import diapi from './src/diapi';
+import { MODES, CLIENTS, DELAY } from './src/consts/constants';
 
 export default diapi;
+export { MODES as Modes, CLIENTS as Clients, DELAY as Delay };
